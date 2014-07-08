bef1d052a489ba8cbd6455ad601329d13f8a9a3b
alberto <alberto.cartagena@gmail.com>
2014-07-08 23:46:18 +0100

    jetty endpoint fixing dependencies for war

---
+++ jetty-endpoint/src/main/java/com/navid/lazylogin/jetty/EmbeddedJetty.java
@@ -0,0 +1,75 @@
+package com.navid.lazylogin.jetty;
+
+
+
+import java.io.IOException;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+public class EmbeddedJetty {
+
+    private static final int DEFAULT_PORT = 8080;
+    private static final String CONTEXT_PATH = "/";
+    private static final String CONFIG_LOCATION = "classpath:conf/config-main.xml";
+    private static final String MAPPING_URL = "/*";
+    private static Server server;
+    private static XmlWebApplicationContext context;
+
+    public static void main(String[] args) throws Exception {
+        new EmbeddedJetty().startJetty(getPortFromArgs(args));
+        server.join();
+    }
+
+    public static WebApplicationContext runServer(final int port) throws Exception {
+        
+        
+        new EmbeddedJetty().startJetty(port);
+        return context;
+    }
+
+    private static int getPortFromArgs(String[] args) {
+        if (args.length > 0) {
+            try {
+                return Integer.valueOf(args[0]);
+            } catch (NumberFormatException ignore) {
+            }
+        }
+        return DEFAULT_PORT;
+    }
+
+    public void startJetty(int port) throws Exception {
+        server = new Server(port);
+        server.setHandler(getServletContextHandler(getContext()));
+        server.start();
+    }
+
+    public static void stopServer() throws Exception {
+        if (server != null) {
+            server.stop();
+        }
+
+    }
+
+    private static ServletContextHandler getServletContextHandler(WebApplicationContext context) throws IOException {
+        ServletContextHandler contextHandler = new ServletContextHandler();
+        contextHandler.setErrorHandler(null);
+        contextHandler.setContextPath(CONTEXT_PATH);
+        contextHandler.addServlet(new ServletHolder(new CXFServlet()), MAPPING_URL);
+
+        contextHandler.addEventListener(new ContextLoaderListener(context));
+        //contextHandler.setResourceBase(new ClassPathResource("webapp").getURI().toString());
+        return contextHandler;
+    }
+
+    private static XmlWebApplicationContext getContext() {
+        context = new XmlWebApplicationContext();
+        context.setConfigLocation(CONFIG_LOCATION);
+        return context;
+    }
+
+}
