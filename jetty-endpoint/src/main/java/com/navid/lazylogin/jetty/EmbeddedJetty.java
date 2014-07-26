d24158b54db761261c6f36951d7cbfc5a5a4071c
casa <casa@localhost>
2014-07-26 17:19:44 +0100

    static info + app version

--- jetty-endpoint/src/main/java/com/navid/lazylogin/jetty/EmbeddedJetty.java
+++ jetty-endpoint/src/main/java/com/navid/lazylogin/jetty/EmbeddedJetty.java
@@ -1,10 +1,18 @@
 package com.navid.lazylogin.jetty;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import org.apache.cxf.transport.servlet.CXFServlet;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler.Context;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
@@ -15,6 +23,7 @@ public class EmbeddedJetty {
     private static final String CONTEXT_PATH = "/";
     private static final String CONFIG_LOCATION = "classpath:conf/config-root.xml";
     private static final String MAPPING_URL = "/*";
+    private static final String STATIC_MAPPING_URL = "/static/*";
     private static Server server;
     private static XmlWebApplicationContext context;
 
@@ -24,8 +33,7 @@ public class EmbeddedJetty {
     }
 
     public static WebApplicationContext runServer(final int port) throws Exception {
-        
-        
+
         new EmbeddedJetty().startJetty(port);
         return context;
     }
@@ -42,7 +50,11 @@ public class EmbeddedJetty {
 
     public void startJetty(int port) throws Exception {
         server = new Server(port);
-        server.setHandler(getServletContextHandler(getContext()));
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{ getServletStaticContextHandler(), getServletContextHandler(getContext())});
+
+        server.setHandler(handlers);
         server.start();
     }
 
@@ -58,10 +70,17 @@ public class EmbeddedJetty {
         contextHandler.setErrorHandler(null);
         contextHandler.setContextPath(CONTEXT_PATH);
         contextHandler.addServlet(new ServletHolder(new CXFServlet()), MAPPING_URL);
-
         contextHandler.addEventListener(new ContextLoaderListener(context));
-        //contextHandler.setResourceBase(new ClassPathResource("webapp").getURI().toString());
+
         return contextHandler;
+    }
+
+    private static Handler getServletStaticContextHandler() throws IOException, URISyntaxException {
+        ResourceHandler resHandler = new ResourceHandler();
+        resHandler.setResourceBase(Resource.newClassPathResource("/static").toString());
+        ContextHandler ctx = new ContextHandler("/static"); /* the server uri path */
+        ctx.setHandler(resHandler);
+        return ctx;
     }
 
     private static XmlWebApplicationContext getContext() {
