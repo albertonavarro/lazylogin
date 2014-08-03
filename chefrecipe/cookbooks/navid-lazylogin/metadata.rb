ddc5a6bf144d02d79ddc304ce3f39f5358a98304
casa <casa@localhost>
2014-08-03 23:29:21 +0100

    simplifying recipe

--- chefrecipe/cookbooks/navid-lazylogin/metadata.rb
+++ chefrecipe/cookbooks/navid-lazylogin/metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures navid-lazylogin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '${project.version}'
 
-%w{ java simple_iptables zabbix }.each do |cb|
+%w{ java }.each do |cb|
   depends cb
 end
--- chefrecipe/cookbooks/navid-lazylogin/recipes/default.rb
+++ chefrecipe/cookbooks/navid-lazylogin/recipes/default.rb
@@ -7,59 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "java"
-include_recipe "simple_iptables"
-include_recipe "zabbix"
-
-# Reject packets other than those explicitly allowed
-simple_iptables_policy "INPUT" do
-  policy "DROP"
-end
-
-# The following rules define a "system" chain; chains
-# are used as a convenient way of grouping rules together,
-# for logical organization.
-
-# Allow all traffic on the loopback device
-simple_iptables_rule "system" do
-  rule "--in-interface lo"
-  jump "ACCEPT"
-end
-
-# Allow any established connections to continue, even
-# if they would be in violation of other rules.
-simple_iptables_rule "system" do
-  rule "-m conntrack --ctstate ESTABLISHED,RELATED"
-  jump "ACCEPT"
-end
-
-# Allow SSH
-simple_iptables_rule "system" do
-  rule "--proto tcp --dport 22"
-  jump "ACCEPT"
-end
-
-# Allow HTTP, HTTPS
-simple_iptables_rule "tomcat" do
-  rule [ "--proto tcp --dport 8080",
-         "--proto tcp --dport 8443" ]
-  jump "ACCEPT"
-end
-
-# Allow HTTP, HTTPS
-simple_iptables_rule "zabbix" do
-  rule [ "--proto tcp --dport 10050",
-         "--proto udp --dport 10050" ]
-  jump "ACCEPT"
-end
-
-# Allow HTTP, HTTPS
-simple_iptables_rule "ajp" do
-  rule [ "--proto tcp --dport 8019",
-         "--proto udp --dport 8019" ]
-  jump "ACCEPT"
-end
-
 remote_file "/root/jetty-deployable.jar" do
    source "http://repo.cabotrafalgar.mooo.com/libs-release-local/com/navid/lazylogin/jetty-endpoint/${project.version}/jetty-endpoint-${project.version}.jar"
 end
