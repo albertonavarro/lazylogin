e1ba4d4f63795e777484a2da57b3d762d443ff7d
alberto <alberto.cartagena@gmail.com>
2014-08-11 23:31:54 +0100

    maybe I should use less data..

--- logic/src/main/java/com/navid/lazylogin/services/SystemServices.java
+++ logic/src/main/java/com/navid/lazylogin/services/SystemServices.java
@@ -3,6 +3,8 @@ package com.navid.lazylogin.services;
 import com.navid.lazylogin.domain.SessionId;
 import com.navid.lazylogin.persistence.Persistence;
 import javax.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,14 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class SystemServices {
-    
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SystemServices.class);
+
     @Resource
     private Persistence persistence;
-    
-    public SessionId getUserInfo( String sessionId ) {
-        SessionId ssoId = persistence.findOneSessionId( sessionId );
+
+    public SessionId getUserInfo(String sessionId) {
+        LOGGER.info("Requesting user info for sessionId {}", sessionId);
         
+        SessionId ssoId = persistence.findOneSessionId(sessionId);
+        
+        LOGGER.debug("Response {}", ssoId);
+
         return ssoId;
     }
-    
+
 }
