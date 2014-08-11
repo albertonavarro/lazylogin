0dd86ec392dd242a9651e8c652add10a3f5d7667
alberto <alberto.cartagena@gmail.com>
2014-08-12 00:11:48 +0100

    Looking for a bug..

--- common-context/src/main/java/com/navid/lazylogin/context/interceptors/RequestIdInterceptor.java
+++ common-context/src/main/java/com/navid/lazylogin/context/interceptors/RequestIdInterceptor.java
@@ -10,8 +10,12 @@ import javax.annotation.Resource;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RequestIdInterceptor extends AbstractPhaseInterceptor<Message> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RequestIdInterceptor.class);
 
     @Resource
     private RequestContextContainer requestContextContainer;
@@ -23,12 +27,14 @@ public class RequestIdInterceptor extends AbstractPhaseInterceptor<Message> {
     @Override
     public void handleMessage(Message message) {
         if (requestContextContainer != null && requestContextContainer.get().getRequestId() != null) {
-                ((Map<String, List<String>>) message.getContextualProperty(Message.PROTOCOL_HEADERS)).put("RID", singletonList(requestContextContainer.get().getRequestId()));
-            
+            ((Map<String, List<String>>) message.getContextualProperty(Message.PROTOCOL_HEADERS)).put("RID", singletonList(requestContextContainer.get().getRequestId()));
+            LOGGER.info("Adding RID header with value {}", requestContextContainer.get().getRequestId());
+        } else {
+            LOGGER.info("RID header not added, requestContextContainer or requestId not found");
         }
     }
-    
-    public void setRequestContextContainer( RequestContextContainer requestContextContainer) {
+
+    public void setRequestContextContainer(RequestContextContainer requestContextContainer) {
         this.requestContextContainer = requestContextContainer;
     }
 }
