2a945043fcfb419fa26172b905c7fc546a3dce70
alberto <alberto.cartagena@gmail.com>
2014-08-14 21:46:02 +0100

    separating RID and SID concepts

--- common-context/src/main/java/com/navid/lazylogin/context/interceptors/AuthorisationFilter.java
+++ common-context/src/main/java/com/navid/lazylogin/context/interceptors/AuthorisationFilter.java
@@ -7,13 +7,17 @@ import com.navid.lazylogin.UserInfo;
 import com.navid.lazylogin.context.RequestContextContainer;
 import java.io.IOException;
 import javax.annotation.Resource;
+import javax.security.sasl.AuthenticationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Built in class for calling Lazylogin system endpoint and retrieve user info
+ */
 public class AuthorisationFilter implements ContainerRequestFilter {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthorisationFilter.class);
 
     @Resource(name = "client")
@@ -24,19 +28,30 @@ public class AuthorisationFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(final ContainerRequestContext requestContext) throws IOException {
-        LOGGER.info("Authorisating request for sessionId: {}", requestContextContainer.get().getSessionId());
-        
-        final UserInfo userInfo = systemCommands.getUserInfo(requestContextContainer.get().getSessionId());
-        
-        LOGGER.debug("UserInfo discovered: {}", userInfo);
-        requestContextContainer.get().setUserId(userInfo.getUsername());
+        String sessionId = requestContextContainer.get().getSessionId();
+                
+        LOGGER.info("Authorisating request for sessionId: {}", sessionId);
+
+        if (requestContextContainer.get().getSessionId() != null) {
+            try {
+                final UserInfo userInfo = systemCommands.getUserInfo(sessionId);
+                LOGGER.debug("UserInfo discovered: {}", userInfo);
+                requestContextContainer.get().setUserId(userInfo.getUsername());
+            } catch (RuntimeException e) {
+                LOGGER.error("Error authenticating with sessionId" + sessionId, e);
+                throw new AuthenticationException("Error authenticating client with sessionId " + sessionId);
+            }
+        } else {
+            throw new AuthenticationException("SessionId can't be null.");
+        }
+
     }
-    
-    public void setSystemCommands( SystemCommands systemCommands ) {
+
+    public void setSystemCommands(SystemCommands systemCommands) {
         this.systemCommands = systemCommands;
     }
-    
-    public void setRequestContextContainer( RequestContextContainer requestContextContainer) {
+
+    public void setRequestContextContainer(RequestContextContainer requestContextContainer) {
         this.requestContextContainer = requestContextContainer;
     }
 
