38855da25dea1453928128096171c97d79f668ed
casa <casa@localhost>
2014-09-24 22:45:09 +0100

    here we are again, fighting against persistence

--- springboot/src/main/java/com/navid/lazylogin/springboot/Application.java
+++ springboot/src/main/java/com/navid/lazylogin/springboot/Application.java
@@ -1,25 +1,36 @@
-
 package com.navid.lazylogin.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@ImportResource({"classpath:/conf/config-web-services.xml","classpath:/conf/config-root.xml"})
 public class Application extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean cxfServlet() {
+        org.apache.cxf.transport.servlet.CXFServlet cxfServlet = new org.apache.cxf.transport.servlet.CXFServlet();
+        ServletRegistrationBean servletDef = new ServletRegistrationBean(cxfServlet, "/services/*");
+        servletDef.setLoadOnStartup(1);
+        return servletDef;
+    }
 
 }
---
+++ springboot/src/main/java/com/navid/lazylogin/springboot/cxf/SystemCommandsImpl.java
@@ -0,0 +1,39 @@
+package com.navid.lazylogin.springboot.cxf;
+
+import com.navid.lazylogin.SystemCommands;
+import com.navid.lazylogin.UserInfo;
+import com.navid.lazylogin.services.SystemServices;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import org.jdto.DTOBinder;
+
+
+@javax.jws.WebService(endpointInterface = "com.navid.lazylogin.SystemCommands")                
+public class SystemCommandsImpl implements SystemCommands {
+
+    private static final Logger LOG = Logger.getLogger(SystemCommandsImpl.class.getName());
+    
+    //@Resource
+    private SystemServices systemServices;
+    
+    @Resource(name = "lazylogin.endpoint.converter")
+    private DTOBinder binder;
+
+    @Override
+    public UserInfo getUserInfo(String sessionId) { 
+        UserInfo userInfo = new UserInfo();
+        
+        if (userInfo == null) {
+            
+        }
+        
+        com.navid.lazylogin.domain.SessionId ssoId = systemServices.getUserInfo(sessionId);
+        
+        //userInfo.setUserid(ssoId.getToken().getUser().getUserId().getValue());
+        userInfo.setVerified(ssoId.getToken().getValidated());
+        //userInfo.setUsername(ssoId.getToken().getUser().getName());
+        
+        return userInfo;
+    }
+
+}
