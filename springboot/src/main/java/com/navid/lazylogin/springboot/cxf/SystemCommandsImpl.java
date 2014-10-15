55dd113b6e78f9ea7dfab291b08bb067641ccedb
casa <casa@localhost>
2014-10-16 00:01:58 +0100

    updating lazylogin

--- springboot/src/main/java/com/navid/lazylogin/springboot/cxf/SystemCommandsImpl.java
+++ springboot/src/main/java/com/navid/lazylogin/springboot/cxf/SystemCommandsImpl.java
@@ -2,6 +2,7 @@ package com.navid.lazylogin.springboot.cxf;
 
 import com.navid.lazylogin.SystemCommands;
 import com.navid.lazylogin.UserInfo;
+import com.navid.lazylogin.domain.SessionId;
 import com.navid.lazylogin.services.SystemServices;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
@@ -21,17 +22,15 @@ public class SystemCommandsImpl implements SystemCommands {
 
     @Override
     public UserInfo getUserInfo(String sessionId) { 
+        SessionId ssoId = systemServices.getUserInfo(sessionId);
+        
         UserInfo userInfo = new UserInfo();
-        
-        if (userInfo == null) {
-            
-        }
-        
-        com.navid.lazylogin.domain.SessionId ssoId = systemServices.getUserInfo(sessionId);
-        
-        //userInfo.setUserid(ssoId.getToken().getUser().getUserId().getValue());
         userInfo.setVerified(ssoId.getToken().getValidated());
-        //userInfo.setUsername(ssoId.getToken().getUser().getName());
+        
+        if(ssoId.getToken().getValidated()){
+            userInfo.setUserid(ssoId.getToken().getUser().getUserId().getValue());
+            userInfo.setUsername(ssoId.getToken().getUser().getName());
+        }
         
         return userInfo;
     }
